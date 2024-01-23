157e9f268f47a93e9461818a0b36648349fa9067
Monky518 <zoeyrizor@gmail.com>
2024-01-23 13:26:23 -0500

    Completed PersonGenerator

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,84 +11,72 @@ public class PersonGenerator
 {
     public static void main(String[] args)
     {
-        JFileChooser chooser = new JFileChooser();
         Scanner scan = new Scanner(System.in);
-        File selectedFile;
-        String rec = "";
-
         ArrayList<String> lines = new ArrayList<>();
         final int FIELDS_LENGTH = 5;
-        String id, firstName, lastName, title;
-        int yob;
-        String regExPattern = "";
-
         boolean done = false;
 
         try
         {
-            File workingDirectory = new File(System.getProperty("user.dir"));
-            chooser.setCurrentDirectory(workingDirectory);      // this is in reading only, but i think it also goes here for safe file selector
-            Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");        // this after chooser to not confuse
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-            {
-                /*
+            /*
                 Your program will prompt the user to enter lines of data for a file on persons
                 You don’t know ahead of time how many Persons will be entered so use an ArrayList to store the records
 
                 Once the user indicates they have entered all the data elements for each person,
                     save it to a text file using a name they provided
                     (Be sure to only save complete sets of elements)
-                 */
+            */
 
-                /*
-                String[] fields;
-                for(String l:lines)
-                {
-                    fields = l.split(","); // Split the record into the fields
-
-                    if(fields.length == FIELDS_LENGTH)
-                    {
-                        id        = fields[0].trim();
-                        firstName = fields[1].trim();
-                        lastName  = fields[2].trim();
-                        title     = fields[3].trim();
-                        yob       = Integer.parseInt(fields[4].trim());
-                        System.out.printf("\n%-8s%-25s%-25s%-6s%6d", id, firstName, lastName, title, yob);
-                    }
-                    else
-                    {
-                        System.out.println("Found a record that may be corrupt: ");
-                        System.out.println(l);
-                    }
-                 }
-                 */
-
-                do
-                {
-                    String userInput = SafeInput.getNonZeroLenString(scan, "Enter line " + lines.length + " of data for the file [type done when done]");
-                    if (userInput.equalsIgnoreCase("done"))
-                    {
-                        done = true;
-                    }
-                    else if (userInput.matches(regExPattern))
-                    {
-                        // YIPPEEEEEE
-                    }
-                    else
-                    {
-                        // trash
-                    }
-
-                } while (!done);
-            }
-            else
+            do
             {
-                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
-            }
+                if (!lines.isEmpty())
+                    ShowLines(lines);
+
+                String userInput = SafeInput.getNonZeroLenString(scan, "Enter your ID, First name, Last name, Title, and Year of birth or type done");
+
+                // checking data
+                String[] test;
+                test = userInput.split(","); // Split the record into the fields
+
+                if (userInput.equalsIgnoreCase("done"))
+                {
+                    done = true;
+
+                    // getting file name and saving it to location
+                    String fileName = SafeInput.getNonZeroLenString(scan,"Enter your new file name");
+                    File workingDirectory = new File(System.getProperty("user.dir"));
+                    Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + fileName + ".txt");
+
+                    // writing the file
+                    OutputStream out =
+                            new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+                    BufferedWriter writer =
+                            new BufferedWriter(new OutputStreamWriter(out));
+
+                    for(String l:lines)
+                    {
+                        writer.write(l, 0, l.length());
+                        writer.newLine();
+                    }
+
+                    writer.close();
+                    System.out.println("Data file written!");
+                }
+                else if (test.length == FIELDS_LENGTH)
+                {
+                    // add data
+                    lines.add(userInput);
+                }
+                else
+                {
+                    System.out.println("\n\nYour input does not match the requirement; try again");
+                }
+
+            } while (!done);
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("File not found!!!");
+            System.out.println("\n\nFile not found");
             e.printStackTrace();
         }
         catch (IOException e)
@@ -100,6 +87,20 @@ public class PersonGenerator
 
     public static void ShowLines(ArrayList<String> arrList)
     {
+        int num = 0;
+        for (String line:arrList)
+        {
+            num++;
+            String[] fields = line.split(",");
 
+            String id        = fields[0].trim();
+            String firstName = fields[1].trim();
+            String lastName  = fields[2].trim();
+            String title     = fields[3].trim();
+            int yob       = Integer.parseInt(fields[4].trim());
+
+            System.out.printf("\nLine %4d | %-8s%-15s%-15s%-6s%6d", num, id, firstName, lastName, title, yob);
+        }
+        System.out.println("\n");
     }
 }
